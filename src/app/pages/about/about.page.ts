c483ffba521f21f622747e8dd2680be9cf0f3d4a
Jeremy Glebe <jeremyglebe@gmail.com>
2019-04-15 11:48:41 -0500

    Started frontend button hiding

--- src/app/pages/about/about.page.ts
+++ src/app/pages/about/about.page.ts
@@ -1,3 +1,4 @@
+import { UsersService } from './../../services/users.service';
 import { BeliefsService } from './../../services/beliefs.service';
 import { AppInfoService } from './../../services/app-info.service';
 import { Component, OnInit } from '@angular/core';
@@ -13,7 +14,8 @@ export class AboutPage implements OnInit {
   constructor(
     public info: AppInfoService,
     public beliefs: BeliefsService,
-    private navCtrl: NavController
+    private navCtrl: NavController,
+    public users: UsersService
   ) { }
 
   ngOnInit() {
